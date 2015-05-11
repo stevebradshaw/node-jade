67ff8e285b8be35430bafc892d6ed7353b92f491
Steve Bradshaw <sjb1970@gmail.com>
2015-05-11 11:14:32 +0100

    Fixed layout so jumbotron is inside container

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-/*
- * Module dependencies
- */
+var appinfo = { port: 3000} ;
+
 var express = require('express')
   , stylus = require('stylus')
   , nib = require('nib')
@@ -17,33 +16,23 @@ function compile(str, path) {
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
-//app.use(express.logger('dev'))
-app.use(logger()) ;
+
+app.use(logger("combined")) ;
+
 app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
   }
 ))
+
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
-  )
-}) ;
-
-app.get('/profile.html', function (req,res) {
-  res.render('profile',
-       { title : 'User Profile' }
-     )
-  }) ;
-
-app.get('/bootstrap.html', function (req,res) {
+app.get('/', function (req,res) {
   res.render('bootstrap',
        { title : 'Bootstrap with jade' }
      )
   }) ;
 
-app.listen(3000, function () {
-console.log('Listening...') ;
+app.listen(appinfo.port, function () {
+  console.log('Listening on port ' + appinfo.port) ;
 })
